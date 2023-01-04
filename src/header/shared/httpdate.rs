1763deba03c003f53b15f213a2d5a6135e09a0ad
hky1999 <976929993@qq.com>
2023-01-04 18:57:50 +0800

    feat: port to shyper

--- src/header/shared/httpdate.rs
+++ src/header/shared/httpdate.rs
@@ -1,7 +1,8 @@
 use std::str::FromStr;
 use std::fmt::{self, Display};
 
-use time;
+// use time;
+use chrono::{DateTime, Utc};
 
 /// A `time::Time` with HTTP formatting and parsing
 ///
@@ -28,25 +29,39 @@ use time;
 //   HTTP-date, the sender MUST generate those timestamps in the
 //   IMF-fixdate format.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct HttpDate(pub time::Tm);
+pub struct HttpDate(pub DateTime<Utc>);
 
 impl FromStr for HttpDate {
     type Err = ::Error;
     fn from_str(s: &str) -> ::Result<HttpDate> {
-        match time::strptime(s, "%a, %d %b %Y %T %Z").or_else(|_| {
-            time::strptime(s, "%A, %d-%b-%y %T %Z")
-            }).or_else(|_| {
-                time::strptime(s, "%c")
-                }) {
-                    Ok(t) => Ok(HttpDate(t)),
-                    Err(_) => Err(::Error::Header),
-                    }
+        // match time::strptime(s, "%a, %d %b %Y %T %Z").or_else(|_| {
+        //     time::strptime(s, "%A, %d-%b-%y %T %Z")
+        //     }).or_else(|_| {
+        //         time::strptime(s, "%c")
+        //         }) {
+        //             Ok(t) => Ok(HttpDate(t)),
+        //             Err(_) => Err(::Error::Header),
+        //             }
+        match DateTime::parse_from_rfc2822(s).or_else(|_| {
+            DateTime::parse_from_rfc3339(s)
+        }).or_else(|_| {
+            DateTime::parse_from_str(s, "%a, %d %b %Y %T %Z")
+       }) .or_else(|_| {
+        DateTime::parse_from_str(s, "%A, %d-%b-%y %T %Z")
+    }) {
+            Ok(t) => {
+                let time = DateTime::<Utc>::from_utc(t.naive_utc(), Utc);
+                Ok(HttpDate(time))
+            },
+            Err(_) => Err(::Error::Header),
+        }
     }
 }
 
 impl Display for HttpDate {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        fmt::Display::fmt(&self.0.to_utc().rfc822(), f)
+        // fmt::Display::fmt(&self.0.to_utc().rfc822(), f)
+        fmt::Display::fmt(&self.0.to_rfc2822(), f)
     }
 }
 
